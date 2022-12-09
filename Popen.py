fa2beabdebd3bd5084de3d398e839df98b8782ed
Hermann Stamm-Wilbrandt <stammw@li-53dd7f4c-320e-11b2-a85c-9651ea6bf510.ibm.com>
2022-12-09 18:23:18 +0100

    Add assert to suqare_sum_prod(), make it return Python list

--- Popen.py
+++ Popen.py
@@ -9,15 +9,19 @@ make clean
 Then [ for 221 = 13*17 = (3²+2²)*(4²+1²) ]:
 
 $ python Popen.py 221
-#3,2,4,1#
+[3, 2, 4, 1]
 $
 
 Any factor 2 or "=3 (mod 4)" will assert in square_sum_prod and abort Python.
+Therefore assert n % 4 == 1 in square_sum_prod() directly.
+This is not sufficient, eg. 3*7=21 will not assert here.
+But without prime factorization cannot be done better for really big numbers.
 """
 import subprocess
 import sys
 
 def square_sum_prod(n):
+    assert n % 4 == 1
     cmd = "square_sum_prod " + str(n) + " py"
 
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -26,7 +30,7 @@ def square_sum_prod(n):
     if proc.returncode != 0:
         return None
 
-    print("#"+str(out[0].decode())+"#")
+    return [int(s) for s in out[0].decode().split(",")]
 
 
-square_sum_prod(sys.argv[1] if len(sys.argv) > 1 else 1105)
+print(square_sum_prod(int(sys.argv[1]) if len(sys.argv) > 1 else 1105))
